fcfba83223b18575eb395f8b9723f6aff61524d9
zieac <ian_geyan@outlook.com>
2023-09-01 15:55:21 +0800

    contract network update

--- src/contracts/config.js
+++ src/contracts/config.js
@@ -1,4 +1,4 @@
-export const CONTRACT_ADDRESS = "0x2a20734277babe8d1caa5900de495301c3111c78";
+export const CONTRACT_ADDRESS = "0x9870584D6d8ca73580347391Ad57E577641654C6";
 export const CONTRACT_ABI = [
 	{
 		"inputs": [
@@ -28,32 +28,34 @@ export const CONTRACT_ABI = [
 	}
 ];
 
-// export const CONTRACT_ADDRESS = "0xfbdbaec336d4aa2c4fd6f36826a0a4128309e1e8";
+
+// export const CONTRACT_ADDRESS = "0x2a20734277babe8d1caa5900de495301c3111c78";
 // export const CONTRACT_ABI = [
-//     {
-//         "inputs": [
-//             {
-//                 "internalType": "uint256",
-//                 "name": "x",
-//                 "type": "uint256"
-//             }
-//         ],
-//         "name": "set",
-//         "outputs": [],
-//         "stateMutability": "nonpayable",
-//         "type": "function"
-//     },
-//     {
-//         "inputs": [],
-//         "name": "get",
-//         "outputs": [
-//             {
-//                 "internalType": "uint256",
-//                 "name": "",
-//                 "type": "uint256"
-//             }
-//         ],
-//         "stateMutability": "view",
-//         "type": "function"
-//     }
+// 	{
+// 		"inputs": [
+// 			{
+// 				"internalType": "uint256",
+// 				"name": "x",
+// 				"type": "uint256"
+// 			}
+// 		],
+// 		"name": "set",
+// 		"outputs": [],
+// 		"stateMutability": "nonpayable",
+// 		"type": "function"
+// 	},
+// 	{
+// 		"inputs": [],
+// 		"name": "get",
+// 		"outputs": [
+// 			{
+// 				"internalType": "uint256",
+// 				"name": "",
+// 				"type": "uint256"
+// 			}
+// 		],
+// 		"stateMutability": "view",
+// 		"type": "function"
+// 	}
 // ];
+
